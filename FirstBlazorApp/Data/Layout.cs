35e1cfb101f21a588e4edd0f1bc7b5c3ddee0e4b
Jarek <jaroslaw.popardowski@wp.pl>
2023-03-20 17:04:28 +0100

    Dodaj pliki projektów.

---
+++ FirstBlazorApp/Data/Layout.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace FirstBlazorApp.Data
+
+{
+    public class Layout
+    {
+        [Required]
+        [StringLength(15, ErrorMessage = "Name should be less than 15 characters")]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(1, 10000, ErrorMessage = "Valid Thick Range is (1-10000)")]
+        public float Thick { get; set; }
+
+        [Required]
+        [Range(1, 10000, ErrorMessage = "Valid Lambda Range is (1-10000)")]
+        public float Lambda { get; set; }
+
+
+        public float FactorR
+        {
+            get; set;
+
+        }
+
+        public void CalculateFR()
+        {
+            FactorR = this.Thick / this.Lambda;
+        }
+
+    }
+}
---
+++ FirstBlazorApp/Data/Wall.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace FirstBlazorApp.Data
+
+
+
+{
+    public class Wall
+    {
+        [Required]
+        [StringLength(15, ErrorMessage = "Name should be less than 15 characters")]
+        public string NameW { get; set; }
+
+
+        public float ThickW { get; set; }
+        public float LambdaW { get; set; }
+
+        public List<Layout> layouts = new();
+
+
+        public float FactorRW
+        {
+            get; set;
+
+        }
+    }
+}
